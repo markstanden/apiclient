23c6ea987877a6d0305e4bb38f1307ce2cd4b038
Mark Standen <no-reply@rundog.dev>
2025-01-07 01:33:22 +0000

    Adds tests for happy path JSON serialisation, and serialization failure

--- ApiClient.Tests.Unit/Services/ExternalHttpServiceTests.cs
+++ ApiClient.Tests.Unit/Services/ExternalHttpServiceTests.cs
@@ -1,5 +1,7 @@
 using System.Net;
 using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using ApiClient.Services;
 using JetBrains.Annotations;
 using NSubstitute;
@@ -17,6 +19,8 @@ public class ExternalHttpServiceTests
         _httpClientWrapper = Substitute.For<IHttpClientWrapper>();
         _sut = new ExternalHttpService(_httpClientWrapper);
     }
+    
+    public record TestData([property: JsonRequired] string Id, [property: JsonRequired] bool ShouldPass);
 
     [Theory]
     [InlineData(null)]
@@ -44,4 +48,34 @@ public class ExternalHttpServiceTests
 
         Assert.IsType<HttpRequestException>(ex);
     }
+
+    [Fact]
+    public async Task GetAsync_WithValidResponse_ReturnsCorrectObject()
+    {
+        var response = new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StringContent("""{"Id":"TestID","ShouldPass":true}""", System.Text.Encoding.UTF8, "application/json")
+        };
+        _httpClientWrapper.GetAsync(Arg.Any<string>()).Returns(response);
+        var expected = new TestData("TestID", true);
+
+        var result = await _sut.GetAsync<TestData>("https://valid.test-url.com");
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public async Task GetAsync_WithIncorrectResponse_ThrowsJsonParseException()
+    {
+        var response = new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StringContent("""{"Testid":"TestID","ShouldPass":false}""", System.Text.Encoding.UTF8, "application/json")
+        };
+        _httpClientWrapper.GetAsync(Arg.Any<string>()).Returns(response);
+
+        var result = await Record.ExceptionAsync(() => _sut.GetAsync<TestData>("https://valid.test-url.com"));
+
+        Assert.IsType<JsonException>(result);
+    }
 }
+
